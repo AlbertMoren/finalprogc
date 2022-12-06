7d599c45136c03e02b204170560ee2346f6962a4
Albert Moren <87720186+AlbertMoren@users.noreply.github.com>
2022-12-06 15:59:26 -0300

    Update TravelBooking.c
    Inserção da Lista encadeada de passageiros, juntamente com as suas funções de acessos

--- TravelBooking.c
+++ TravelBooking.c
@@ -44,21 +44,6 @@ struct lista_voo {
     struct no_voo *primeiro;
 };
 
-struct passageiro {
-    int id;
-    char *nome;
-    char *endereco;
-};
-
-struct no_passageiro {
-    Passageiro *passageiro;
-    struct no_passageiro *proximo;
-};
-
-struct lista_passageiro {
-    struct no_passageiro *primeiro;
-};
-
 struct reserva{
     int codigo;
     Data *data_viagem;
@@ -321,7 +306,236 @@ Agenda *Edita(Agenda *raiz, int codEditar){
     /*I- Achar agenda; II- Criar Cópia; III- Remover Agenda original, IV- Inserir cópia modificada*/
     /*boooool*/
 }
+//=-=-=-=-=-=-=-=-=-=-=-=Lista encadeada de passageiros=-=-=-=-=-=
 
+struct passageiro {
+  int id;
+  char *nome;
+  char *endereco;
+};
+
+struct no_passageiro {
+  Passageiro *passageiro;
+  struct no_passageiro *proximo;
+};
+
+struct lista_passageiro {
+  struct no_passageiro *primeiro;
+};
+
+typedef struct no_passageiro No_Passagueiro;
+
+/* Aloca espaço em memória e retorna uma Lista */
+ListaPassageiro *lista_cria(){
+  ListaPassageiro *lista = (ListaPassageiro *)malloc(sizeof(ListaPassageiro));
+  lista->primeiro = NULL;
+  return lista;
+}
+
+/* Libera a memória da lista previamente criada e atribui NULL ao ponteiro lista.
+ * Retorna 1 caso seja possível fazer a liberação e 0 caso a lista informada seja NULL. */
+int lista_liberar(ListaPassageiro **lista){
+  if(lista != NULL){
+    if((*lista)->primeiro != NULL){
+      No_Passagueiro *aux = (*lista)->primeiro;
+      do{
+        passageiro_libera(&aux->passageiro);
+        aux = aux->proximo;
+      }while(aux != NULL);
+    }
+    free(*lista);
+    *lista = NULL;
+    return 1;
+  }
+  return 0;
+}
+
+/* Insere um passageiro na lista.
+ *  Retorna 1 se foi possível adicionar, 0 caso já exista um passageiro com o mesmo id (nesse caso, o passageiro não pode ser adicionado) e -1 caso a lista ou passageiro sejam NULL */
+int lista_inserir(ListaPassageiro *lista, Passageiro *passageiro){
+  if(lista == NULL || passageiro==NULL){
+    return -1;
+  }
+
+  if(lista->primeiro != NULL){
+    int id_passagueiro;
+    char nome[50];
+    char endereco[50];
+    passagueiro_acessa(passageiro, &id_passagueiro,nome,endereco);
+    Passageiro *passageiro_aux = passageiro_busca(lista,id_passagueiro);
+    if(passageiro_aux != NULL){
+      return 0;
+    }
+  }
+
+  if(lista->primeiro == NULL){
+    No_Passagueiro *novo = (No_Passagueiro *)malloc(sizeof(No_Passagueiro));
+    novo->passageiro = passageiro;
+    novo->proximo = NULL;
+    lista->primeiro = novo;
+  }else{
+    No_Passagueiro *ultimo = lista->primeiro;
+    while (ultimo->proximo != NULL){
+      ultimo = ultimo->proximo;
+    }
+
+    No_Passagueiro *novo = (No_Passagueiro *)malloc(sizeof(No_Passagueiro));
+    novo->passageiro = passageiro;
+    novo->proximo = NULL;
+    ultimo->proximo = novo;
+  }
+  return 1;
+}
+
+/* Busca passageiro pelo número de id. Retorna o passageiro se este estiver na
+ * lista e NULL caso contrário, isto é, (i) lista vazia; (ii) não exista passageiro com a id fornecida;
+ * ou (iii) a lista seja NULL */
+ListaPassageiro *lista_busca(ListaPassageiro *lista, int ID){
+  if(lista != NULL && lista->primeiro != NULL){
+
+    int passageiro_id;
+    char nome[50];
+    char endereco[30];
+
+    No_Passagueiro *aux = lista->primeiro;
+    do{
+      passagueiro_acessa(aux->passageiro, &passageiro_id, nome, endereco);
+      if(ID == passageiro_id){
+        return aux->passageiro;
+      }
+      aux = aux->proximo;
+    }while(aux != NULL);
+  }
+
+  return NULL;
+}
+
+/* Remove um passagueiro na lista. Retorna o passagueiro ou NULL caso a lista esteja vazia*/
+ListaPassageiro *lista_retirar(ListaPassageiro *lista){
+  if(lista == NULL || lista->primeiro == NULL){
+    return NULL;
+  }
+  Passageiro *passageiro = lista->primeiro->passageiro;
+  lista->primeiro = lista->primeiro->proximo;
+  return passageiro;
+}
+
+/* Verifica se a lista está vazia. Retorna 1 se a lista estiver vazia, 0 caso não
+ * esteja vazia e -1 se a lista for NULL*/
+int lista_vazia(ListaPassageiro* lista){
+  if (lista==NULL){
+    return -1;
+  }
+  if (lista->primeiro==NULL){
+    return 1;
+  }
+  return 0;
+}
+
+/* Recupera o primeiro passageiro da fila. Retorna o passageiro ou NULL caso a lista esteja vazia ou seja NULL */
+Passageiro *lista_primeiro(ListaPassageiro* lista){
+  if(lista == NULL || lista->primeiro->passageiro ==NULL){
+    return NULL;
+  }
+  return lista->primeiro->passageiro;
+}
+
+
+/*=-=-=-=-=-=-=-=-=-=-=-=-= funções para a lista de passageiros=-=-=-=-=-=-=-=-*/
+
+int verificar_parametros(int passageiro_id, char *nome, char *endereco){
+    if(passageiro_id < 0 || nome == NULL || endereco == NULL){
+        return -1;
+    }
+
+    if(strlen(nome) > 50 || strlen(endereco) > 30){
+        return -1;
+    }
+
+    return 1;
+}
+
+/* Aloca e retorna um aluno com os dados passados por parâmetro. Porém, para os
+ * casos em que (i) pelo menos um dos parâmetros sejam nulos <-1, NULL, NULL>; e
+ * (ii) o tamanho das strings nome e curso sejam maiores que os da especificação
+ * (50 e 30, respectivamente), a função deve retornar NULL. */
+Passageiro *passageiro_novo(int id, char *nome,char *endereco){
+  if(verificar_parametros(id,nome,endereco)== -1){
+    return NULL;
+  }
+  if(strcmp(nome,endereco) == 0){
+    return NULL;
+  }
+  Passageiro *novo_passageiro = (Passageiro *)malloc(sizeof(Passageiro));
+  novo_passageiro->id = id;
+  strcpy(novo_passageiro->nome, nome);
+  strcpy(novo_passageiro->endereco, endereco);
+
+  return novo_passageiro;
+}
+
+/* Libera a memória de um aluno previamente criado e atribui NULL ao aluno. */
+void passageiro_liberar(Passageiro **passageiro){
+  if(passageiro != NULL){
+    free(passageiro);
+    *passageiro = NULL;
+  }
+}
+
+/* Copia os valores de um aluno para as referências informadas. Em caso de aluno
+ * NULL, atribuir valor padrão <-1, "NULL", "NULL"> aos parâmetros. */
+void passageiro_acessa(Passageiro *passageiro,int *id, char *nome, char *endereco){
+  if(passageiro == NULL){
+    *id = -1;
+    strcpy(nome, "NULL");
+    strcpy(endereco, "NULL");
+  }else{
+    *id = passageiro->id;
+    strcpy(nome, passageiro->nome);
+    strcpy(endereco, passageiro->endereco);
+  }
+}
+
+/* Atribui novos valores aos campos de um aluno. Porém, para os casos em que (i)
+ * algum dos parâmetros sejam nulos <NULL, -1, NULL, NULL>; ou (ii) o tamanho
+ * das strings nome e curso sejam maiores que os da especificação (50 e 30,
+ * respectivamente), a função não deve fazer a atribuição. */
+void passageiro_atribuir(Passageiro *passageiro, int id, char *nome, char *endereco){
+  if(passageiro != NULL && verificar_parametros(id,nome,endereco) == 1){
+    passageiro->id = id;
+    strcpy(passageiro->nome, nome);
+    strcpy(passageiro->endereco, endereco);
+  }
+}
+
+/* Avalia se dois alunos são iguas. A função deve retornar 1 se os alunos
+ * possuem os mesmos dados, 0 caso os dados dos alunos possuam alguma diferença
+ * e -1 caso pelo menos um dos alunos seja NULL.
+ */
+int passageiro_igual(Passageiro *passageiro1, Passageiro *passageiro2){
+  if(passageiro1 == NULL || passageiro2 == NULL){
+    return -1;
+  }
+
+  if(passageiro1->id != passageiro2->id){
+    return 0;
+  }
+
+  if(strcmp(passageiro1->nome,passageiro2->nome) != 0){
+    return 0;
+  }
+
+  if(strcmp(passageiro1->endereco,passageiro2->endereco) != 0){
+    return 0;
+  }
+
+  return 1;
+}
+
+/* Retorna o tamanho em bytes do TAD aluno. */
+int passageiro_tramanho() {
+  return sizeof(Passageiro); 
+}
 
 
 //int arvore;
